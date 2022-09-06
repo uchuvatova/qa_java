9dc1758d8ebf67cb8d858657fdc7e4f0da58433c
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-06 22:47:23 +0300

    Добавлен тест на выброс исключений

--- src/test/java/FoodTest.java
+++ src/test/java/FoodTest.java
@@ -1,9 +1,11 @@
 import com.example.Animal;
+import com.example.Lion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @RunWith(Parameterized.class) // Напиши аннотацию для параметризованных тестов
 public class FoodTest {
@@ -21,11 +23,19 @@ public class FoodTest {
                 { List.of("Трава", "Различные растения"), "Травоядное"},
                 { List.of("Животные", "Птицы", "Рыба"), "Хищник"},};
     }
-
     @Test
     public void getFoodSomeAnimalKindShowsFood() throws Exception {
         Animal animal = new Animal();
         List<String> actual = animal.getFood(animalKind);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void incorrectAnimalKindShouldThrowException()  {
+        Exception e = assertThrows(Exception.class, () -> {
+            Animal animal = new Animal();
+            animal.getFood("Всеядное");});
+        String expected = "Неизвестный вид животного, используйте значение Травоядное или Хищник";
+        String actual = e.getMessage();
+        assertEquals(expected, actual);}
 }
