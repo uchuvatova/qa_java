df0ab49a7d4129066d05d3910978477313cbf6be
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-06 22:39:59 +0300

    Параметр "Самец" используется в методе getkittens()

--- src/main/java/com/example/Alex.java
+++ src/main/java/com/example/Alex.java
@@ -7,15 +7,12 @@ public class Alex extends Lion {
     public Alex(String sex) throws Exception {
         super("Самец");
     }
+    @Override
+    public int getKittens() {
+        return kittensCount;}
+
     public List<String> getFriends() { return List.of("Марти", "Глория", "Мелман");}
 
     public String getPlaceOfLiving() { return "Нью-Йоркский зоопарк";}
 
-    @Override
-    public int getKittens() {
-        return getKittens(0);
     }
-
-
-}
-
