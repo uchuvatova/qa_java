71c9591484b8c55c20e802d05fed20492ee36f69
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-06 22:45:42 +0300

    Исправлен метод мока

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -3,19 +3,21 @@ import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-
     @Mock
     Feline feline; // создали мок
     @Test
     public void checkCatIsEatingMeat() throws Exception {
-       Cat cat = new Cat(feline);
-        assertEquals(cat.getFood(), feline.eatMeat());}
-
+        Cat cat = new Cat(feline);
+        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), cat.getFood());
+    }
 
     @Test
     public void checkCatGetSoundMeow() {
