102e6a206f26854f6f051315e724f700d146402b
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-08 01:21:10 +0300

    Поставила на место expected и actual

--- src/test/java/AlexTest.java
+++ src/test/java/AlexTest.java
@@ -1,23 +1,27 @@
 import com.example.Alex;
+import com.example.Feline;
 import com.example.Lion;
 import org.junit.Test;
+import org.mockito.Mock;
+
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 
 public class AlexTest {
-
+    @Mock
+    Feline feline;
     @Test
     public void checkPlaceOfLivingIsNewYorkZoo() throws Exception {
-        Alex alex = new Alex("Самец");
-        assertEquals(alex.getPlaceOfLiving(), "Нью-Йоркский зоопарк");}
+        Alex alex = new Alex(feline);
+        assertEquals("Нью-Йоркский зоопарк", alex.getPlaceOfLiving());}
 
     @Test
     public void checkFriendsAreMartieGloriaMelman() throws Exception {
-        Alex alex = new Alex("Самец");
-        assertEquals(alex.getFriends(), List.of("Марти", "Глория", "Мелман"));}
+        Alex alex = new Alex(feline);
+        assertEquals(List.of("Марти", "Глория", "Мелман"), alex.getFriends());}
 
     @Test
     public void getKittensAlexZeroAmountShowsOk() throws Exception {
-        Alex alex = new Alex("Самец");
-        assertEquals(alex.getKittens(), 0);}
+        Alex alex = new Alex(feline);
+        assertEquals(0, alex.getKittens());}
 }
