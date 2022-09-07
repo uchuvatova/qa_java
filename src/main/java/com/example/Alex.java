5cdf4d602730f3827af7a358043ec7506bb82565
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-08 01:18:26 +0300

    Убрала параметр sex

--- src/main/java/com/example/Alex.java
+++ src/main/java/com/example/Alex.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 public class Alex extends Lion {
 
-    public Alex(String sex) throws Exception {
-        super("Самец");
+    private  Feline feline;
+
+    public Alex(Feline feline) throws Exception {
+        super("Самец",feline);
     }
     @Override
     public int getKittens() {
-        return kittensCount;}
+        return 0;}
 
     public List<String> getFriends() { return List.of("Марти", "Глория", "Мелман");}
 
     public String getPlaceOfLiving() { return "Нью-Йоркский зоопарк";}
-
-    }
+}
