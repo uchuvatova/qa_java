c38d845ddac5ca1aeba357460bbc711bf5516441
Ira Uchuvatova <i.uchuvatova@yandex.ru>
2022-09-08 22:15:14 +0300

    После исправления изоляции Lion от Feline поправила тесты методов getKittens() и getFood() (добавила моки)

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -1,28 +1,30 @@
 import com.example.*;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
+import org.mockito.junit.MockitoJUnitRunner;
 import java.util.List;
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class LionTest {
         @Mock
         Feline feline;
+
         @Test
-        public void testGetKittensLion() throws Exception {
-                //int expectedResult = 1;
+        public void getKittensShouldReturnOneKitten() throws Exception {
                 Lion lion = new Lion("Самец", feline);
                 Mockito.when(feline.getKittens()).thenReturn(1);
-                int actualResult = lion.getKittens();
-                assertEquals(1, actualResult);
+                int expected = 1;
+                int actual = lion.getKittens();
+                assertEquals(expected, actual);
         }
 
         @Test
         public void incorrectLionSexShouldThrowException()  {
-                Exception e = assertThrows(Exception.class, () -> {Lion lion = new Lion("Male", feline);});
+                Exception e = assertThrows(Exception.class, () -> {
+                        Lion lion = new Lion("Male", feline);});
                 String expected = "Используйте допустимые значения пола животного - самец или самка";
                 String actual = e.getMessage();
                 assertEquals(expected, actual);}
@@ -30,6 +32,9 @@ public class LionTest {
         @Test
         public void checkLionIsEatingMeat() throws Exception {
                 Lion lion = new Lion("Самец", feline);
-                assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
+                List<String> expected = List.of("Животные", "Птицы", "Рыба");
+                Mockito.when(feline.getFood("Хищник")).thenReturn(expected);
+                List<String> actual = lion.getFood();
+                assertEquals(expected, actual);
                 }
         }
